16e2ea3123f44ed992b033ddb5f1587be9c14569
ContratopDev <Contratop@molecule.net>
2021-09-26 00:49:55 +0000

    FTP Code Changed

--- .simpleapp/ftpserver.py
+++ .simpleapp/ftpserver.py
@@ -3,27 +3,8 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-print('WhiteDragon FTP Server')
-print('')
-def yes_or_no(question):
-    reply = str(raw_input(question+'Deseas usar el puerto 8021? (y/n): ')).lower().strip()
-    if reply[0] == 'y':
-        FTP_PORT = 8021
-    if reply[0] == 'n':
-        FTP_PORT = raw_input("Escriba el puerto a usar: ")
-    else:
-        FTP_PORT = 8021
 
-def yes_or_no(question):
-    reply = str(raw_input(question+'Deseas usar el directorio standard? (y/n): ')).lower().strip()
-    if reply[0] == 'y':
-        FTP_DIRECTORY = os.getcwd()
-    if reply[0] == 'n':
-        FTP_DIRECTORY = raw_input("Escriba el directorio a usar: ")
-    else:
-        FTP_DIRECTORY = os.getcwd()
-
-
+FTP_PORT = 8021
 FTP_DIRECTORY = os.getcwd()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
